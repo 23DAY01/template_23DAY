b3f4c8adc2f08e1251a02dc21c812d1b27227a49
23DAY01 <1784247914@qq.com>
2022-10-12 14:06:51 +0800

    init commit

---
+++ src/main/java/site/day/template/Template23DayApplication.java
@@ -0,0 +1,13 @@
+package site.day.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Template23DayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Template23DayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/day/template/Template23DayApplicationTests.java
@@ -0,0 +1,13 @@
+package site.day.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Template23DayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
